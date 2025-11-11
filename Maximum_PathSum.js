f0abc0a8a9f4f40a7e5e2fc729083debf9ba8537
Ujwala <ujwalap801@gmail.com>
2025-11-11 21:07:21 +0530

    Added Maximum PathSum.js file

---
+++ Maximum_PathSum.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function(root) {
+
+    let maxSumPath =-Infinity;
+    let traversal =(curr)=>
+    {
+     if(!curr) return 0;
+     let maxLeft = Math.max(0, traversal(curr.left));
+     let maxRight = Math.max(0, traversal(curr.right));
+
+     currMax = curr.val + maxLeft+maxRight;
+     maxSumPath = Math.max(currMax, maxSumPath);
+     return curr.val + Math.max(maxLeft,maxRight);
+
+    }
+
+    traversal(root);
+    return maxSumPath;
+    
+};
